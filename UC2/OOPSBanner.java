7d4a8554f8efea9602f8527272e13e3494cd5edb
Prince Kumar <pk8894715@gmail.com>
2026-03-17 14:18:58 +0530

    UC2 - banner print

---
+++ UC2/OOPSBanner.java
@@ -0,0 +1,14 @@
+//package UC2;
+public class OOPSBanner {
+    public static void main(String[] args){
+        System.out.println("   ***   " + " " +"   ***   "+" "+"******   "+" "+"   *****");
+        System.out.println(" **   ** " + " " +" **   ** "+" "+"**    ** "+" "+" **     ");
+        System.out.println("**     **" + " " +"**     **"+" "+"**     **"+" "+"**      ");
+        System.out.println("**     **" + " " +"**     **"+" "+"**    ** "+" "+" **     ");
+        System.out.println("**     **" + " " +"**     **"+" "+"******   "+" "+"   ***  ");
+        System.out.println("**     **" + " " +"**     **"+" "+"**       "+" "+"     ** ");
+        System.out.println("**     **" + " " +"**     **"+" "+"**       "+" "+"      **");
+        System.out.println(" **   ** " + " " +" **   ** "+" "+"**       "+" "+"     ** ");
+        System.out.println("   ***   " + " " +"   ***   "+" "+"**       "+" "+" *****");
+}
+}
